cc5dc377e1115d9d1e5997a23d45c7b1e32eda49
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2023-08-29 13:35:09 -0300

    TP01 q01-q04 100%

--- AEDSII/TP01/q03/q03.java
+++ AEDSII/TP01/q03/q03.java
@@ -1,23 +1,28 @@
-import java.util.*;
-
 class q03 {
     public static void main(String[] args) {
         MyIO myIo = new MyIO();
-        //myIo.setCharset("UTF-8");
         String input;
+        
         do {
             input = myIo.readLine();
-            char[] cifra = new char[input.length()];
+            
             if (!input.equalsIgnoreCase("FIM")) {
-               encrypt(input, cifra);
-               myIo.println(new String(cifra));
+                String encryptedText = encrypt(input);
+                //myIo.setCharset("ASCII");
+                myIo.println(encryptedText);
+                //System.out.println(encryptedText);
             }
         } while (!input.equalsIgnoreCase("FIM"));
     }
 
-    public static void encrypt(String input, char[] s) {
-        for (int i = 0; i < input.length(); i++) {   
-                s[i] = (char) (input.charAt(i) + 3);
+    public static String encrypt(String input) {
+        String encryptedText = "";
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char encryptedChar = (char)(currentChar + 3);
+
+            encryptedText += encryptedChar;
         }
+        return encryptedText;
     }
 }
---
+++ AEDSII/TP01/q03/q03TesteGustavo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class q03TesteGustavo {
+    public static void main(String[] args) {
+        MyIO myIo = new MyIO();
+        //myIo.setCharset("UTF-8");
+        String input;
+        do {
+            input = myIo.readLine();
+            char[] cifra = new char[input.length()];
+            if (!input.equalsIgnoreCase("FIM")) {
+               encrypt(input, cifra);
+               myIo.println(new String(cifra));
+            }
+        } while (!input.equalsIgnoreCase("FIM"));
+    }
+
+    public static void encrypt(String input, char[] s) {
+        for (int i = 0; i < input.length(); i++) {   
+                s[i] = (char) (input.charAt(i) + 3);
+        }
+    }
+}
