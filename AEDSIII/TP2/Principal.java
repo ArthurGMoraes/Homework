8c38786dd56508a6060556aafb112f6d5ca0d5d6
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2024-05-05 17:33:27 -0300

    alteração de pasta

--- AEDSIII/TP2/Principal.java
+++ AEDSIII/TP2/Principal.java
@@ -1,75 +1,116 @@
-import java.io.*;
-import java.util.*;
 
-import aed3.Arquivo;
+import java.io.File;
+import java.util.Scanner;
 
-class Principal {
+import arquivos.ArquivoAutores;
+import arquivos.ArquivoCategorias;
+import arquivos.ArquivoLivros;
+import entidades.Autor;
+import entidades.Categoria;
+import entidades.Livro;
 
-  public static void main(String args[]) {
-    Scanner console = new Scanner(System.in);
+public class Principal {
 
-    new File("dados/livros.db").delete();
-    new File("dados/livros.hash_d.db").delete();
-    new File("dados/livros.hash_c.db").delete();
-    new File("dados/dicionario.listainv.db").delete();
-    new File("dados/blocos.listainv.db").delete();
+  private static Scanner console = new Scanner(System.in);
 
-    Arquivo<Livro> arqLivros;
-    Livro l1 = new Livro(-1, "9788563560278", "Bangas Oi com", 15.99F);
-    Livro l2 = new Livro(-1, "9788584290482", "Com Bangas Anao", 39.90F);
-    Livro l3 = new Livro(-1, "9786559790005", "Modernidade Líquida", 48.1F);
-    Livro l4 = new Livro(-1, "9788582714911", "Memória", 55.58F);
-    Livro l5 = new Livro(-1, "9786587150062", "Com Amor", 48.9F);
-    int id1, id2, id3, id4, id5;
+  public static void main(String[] args) {
 
     try {
-      arqLivros = new Arquivo<>("livros", Livro.class.getConstructor());
 
-      id1 = arqLivros.create(l1);
-      System.out.println("Livro criado com o ID: " + id1);
+      int opcao;
+      do {
+        System.out.println("\n\n\nBOOKAEDS 1.0");
+        System.out.println("------------");
+        System.out.println("\n> Início");
+        System.out.println("\n1) Categorias");
+        System.out.println("2) Autores");
+        System.out.println("3) Livros");
+        System.out.println("\n9) Reiniciar BD");
+        System.out.println("\n0) Sair");
 
-      id2 = arqLivros.create(l2);
-      System.out.println("Livro criado com o ID: " + id2);
+        System.out.print("\nOpção: ");
+        try {
+          opcao = Integer.valueOf(console.nextLine());
+        } catch (NumberFormatException e) {
+          opcao = -1;
+        }
 
-      id3 = arqLivros.create(l3);
-      System.out.println("Livro criado com o ID: " + id3);
-
-      id4 = arqLivros.create(l4);
-      System.out.println("Livro criado com o ID: " + id4);
-
-      id5 = arqLivros.create(l5);
-      System.out.println("Livro criado com o ID: " + id5);
-
-      // if (arqLivros.delete(id2))
-      //   System.out.println("Livro de ID " + id2 + " excluído!");
-      // else
-      //   System.out.println("Livro de ID " + id2 + " não encontrado!");
-
-      l4.setTitulo("A Memória");
-      if (arqLivros.update(l4))
-        System.out.println("Livro de ID " + l4.getID() + " alterado!");
-      else
-        System.out.println("Livro de ID " + l4.getID() + " não encontrado!");
-
-
-      // System.out.println("\nLivro 3:\n" + arqLivros.read(3));
-      // System.out.println("\nLivro 1:\n" + arqLivros.read(1));
-      // System.out.println("\nLivro 5:\n" + arqLivros.read(5));
-      // System.out.println("\nLivro 4:\n" + arqLivros.read(4));
-      // System.out.println("\nLivro 2:\n" + arqLivros.read(2));
-
-      System.out.println("\nBUSCA");
-      System.out.print("Chave: ");
-      String chave = console.nextLine();
-      arqLivros.buscar(chave);
-
-      arqLivros.close();
-      console.close();
+        switch (opcao) {
+          case 1:
+            (new MenuCategorias()).menu();
+            break;
+          case 2:
+            (new MenuAutores()).menu();
+            break;
+          case 3:
+            (new MenuLivros()).menu();
+            break;
+          case 9:
+            preencherDados();
+            break;
+          case 0:
+            break;
+          default:
+            System.out.println("Opção inválida");
+        }
+      } while (opcao != 0);
 
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
 
+  public static void preencherDados() {
+    try {
+      new File("dados/categorias.db").delete();
+      new File("dados/categorias.hash_d.db").delete();
+      new File("dados/categorias.hash_c.db").delete();
+      new File("dados/autores.db").delete();
+      new File("dados/autores.hash_d.db").delete();
+      new File("dados/autores.hash_c.db").delete();
+      new File("dados/livros.db").delete();
+      new File("dados/livros.hash_d.db").delete();
+      new File("dados/livros.hash_c.db").delete();
+      new File("dados/livros_isbn.hash_d.db").delete();
+      new File("dados/livros_isbn.hash_c.db").delete();
+      new File("dados/livros_categorias.btree.db").delete();
+      new File("dados/dicionario.listainv.db").delete();
+      new File("dados/blocos.listainv.db").delete();
+
+      ArquivoLivros arqLivros = new ArquivoLivros();
+      ArquivoCategorias arqCategorias = new ArquivoCategorias();
+      ArquivoAutores arqAutores = new ArquivoAutores();
+
+      arqCategorias.create(new Categoria("Romance"));
+      arqCategorias.create(new Categoria("Educação"));
+      arqCategorias.create(new Categoria("Sociologia"));
+      arqCategorias.create(new Categoria("Policial"));
+      arqCategorias.create(new Categoria("Aventura"));
+      arqCategorias.create(new Categoria("Suspense"));
+
+      arqAutores.create(new Autor("Homero"));
+      arqAutores.create(new Autor("Lilian Bacich"));
+      arqAutores.create(new Autor("Adolfo Tanzi Neto"));
+      arqAutores.create(new Autor("Zygmunt Bauman"));
+      arqAutores.create(new Autor("Plínio Dentzien"));
+      arqAutores.create(new Autor("Ivan Izquierdo"));
+      arqAutores.create(new Autor("Mariana Zapata"));
+
+      arqLivros.create(new Livro("9788563560278", "Odisseia", 15.99F, 1));
+      arqLivros.create(new Livro("9788584290483", "Ensino Híbrido", 39.90F, 2));
+      arqLivros.create(new Livro("9786559790005", "Modernidade Líquida", 48.1F, 3));
+      arqLivros.create(new Livro("9788582714911", "Memória", 55.58F, 1));
+      arqLivros.create(new Livro("9786587150062", "Com Amor", 48.9F, 1));
+
+      arqLivros.close();
+      arqCategorias.close();
+      arqAutores.close();
+
+      System.out.println("Banco de dados reinicializado com dados de exemplo.");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
