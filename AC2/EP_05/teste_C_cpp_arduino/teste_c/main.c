1ad7fc6353f2a368bfb704c05cee9a5a41a04308
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2024-05-09 21:10:49 -0300

    EP05

--- AC2/EP_05/teste_C_cpp_arduino/teste_c/main.c
+++ AC2/EP_05/teste_C_cpp_arduino/teste_c/main.c
@@ -5,38 +5,21 @@
 int main()
 {
     clock_t inicio, fim, T;
-    float Tempo, mediaC=0,mediaI=0,mediaF=0;
-    register c;
-    char *ic, *jc,xc=3,yc=1;
-    ic=&xc;
-    jc=&yc;
-
-    int *ii, *ji,xi=3,yi=1;
-    ii=&xi;
-    ji=&yi;
-
-    float *ifl, *jf,xf=3,yf=1;
-    ifl=&xf;
-    jf=&yf;
+    float Tempo, media=0;
+    int c;
+    float *i, *j,x=3,y=1;
+    i=&x;
+    j=&y;
 
     int k, num1=1, num2=3;
     T=CLOCKS_PER_SEC;
     for (k=1;k<=10;k=k+1)
     {   inicio=clock();
-        for (c=1;c<=10000000;c=c+1);
+        for (c=1;c<=10000000;c=c+1)*i = *i + *j;
         fim = clock();
         Tempo =( (fim - inicio)*1000/CLOCKS_PER_SEC);
         printf("\nTempo : %g ms.", Tempo);
-        mediaC=mediaC+Tempo;
+        media=media+Tempo;
     }
-
-    for (k=1;k<=10;k=k+1)
-    {   inicio=clock();
-        for (c=1;c<=10000000;c=c+1);
-        fim = clock();
-        Tempo =( (fim - inicio)*1000/CLOCKS_PER_SEC);
-        printf("\nTempo : %g ms.", Tempo);
-        mediaC=mediaC+Tempo;
-    }
-    printf("\nTempo gasto media: %g ms.", mediaC/10);
+    printf("\nTempo gasto media: %g ms.", media/10);
 }
